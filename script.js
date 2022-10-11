fddf2790006ea73f5436b787ae3a2df59a081ee0
Alfa Marine <alfamarine.ferreira@gmail.com>
2022-10-11 19:20:17 -0300

    add square with rainbow effect

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-let message = "Teste";
+// const canvas = document.getElementById("canvas");
+
+// canvas.style.backgroundImage = "url('./images/plant.png')";
+// canvas.style.backgroundSize = "100px";
+// canvas.style.backgroundColor = "#1b263b";
+
+const pix = document.getElementById("square");
+
+// pix.style.backgroundImage = "url('./images/plant.png')";
+// pix.style.backgroundSize = "100px";
+// pix.style.backgroundColor = "#transparent";
